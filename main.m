abfb19becbc7f9cb379f14c78b2e93bb5274301a
nickchu35 <nickchu35@gmail.com>
2015-04-16 21:26:39 +0800

    finished cylinrical reprojection

--- main.m
+++ main.m
@@ -1,10 +1,10 @@
 %% clear previous records and variables
 clear;
 clc;
-%% Main file for HW2 - image stitching
+%% Main file for HW2 - Image Stitching
 %% Read in photos
 dirName = 'test_photos';
-file = dir([dirName '/' '*.JPG']);
+file = dir([dirName '/' '*.jpg']);
 img = {};
 disp('Reading images...');
 tic;
@@ -13,13 +13,25 @@ for k = 1 : size(file,1)
 end
 disp(['Reading images finished... ' int2str(size(file,1)) ' images found.']);
 toc;
+%% Reprojection to cylinder for each Image
+disp('Cylinder reprojection start......');
+tic;
+focal_length = get_focal_length(size(file,1));
+cylin_img = {};
+cylin_path = 'cylin_photos/cylin_';
+for k = 1 : size(file,1) 
+    cylin_img{k} = cylinder_reproject(img{k}, size(img{k},2), size(img{k},1), focal_length);
+    imwrite(cylin_img{k},[cylin_path int2str(k) '.jpg'])
+end
+disp('Cylinder reprojection finished!');
+toc;
 %% Feature Detection
 disp('Running Harris Corner Detection...');
 tic;
-corner_bin_im = zeros(size(img{1},1),size(img{1},2));
+corner_bin_im = zeros(size(cylin_img{1},1),size(cylin_img{1},2));
 rows = [];
 cols = [];
-[corner_bin_im rows cols] = harris(img{1}, 2, 1000, 2, 1); % harris(im, sigma, thresh, radius, disp)
+[corner_bin_im rows cols] = harris(cylin_img{1}, 2, 1000, 2, 1); % harris(im, sigma, thresh, radius, disp)
 figure;
 imshow(corner_bin_im);
 disp('rows : '); disp(size(rows,1));
@@ -28,6 +40,5 @@ disp('Harris Corner Detection... Done');
 toc;
 %% Feature Descriptor
 %% Feature Matching
-%% Reprojection to cylinder
-%% Image Alignment
+%% Image Alignment (matching, find transformation matrix)
 %% Panoramas
